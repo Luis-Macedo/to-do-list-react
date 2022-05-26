2cad332e55cb240bf815134a8a0a2a7383bc754b
= <luismacedo2001@outlook.com>
2022-05-26 16:41:09 -0300

    Done

--- to-do-list/src/Components/Lista/Item/index.tsx
+++ to-do-list/src/Components/Lista/Item/index.tsx
@@ -1,12 +1,29 @@
 import { ITarefa } from '../../../Types/tarefa'
-import style from '../Lista.module.scss'
+import style from './Item.module.scss'
 
-export default function item({tarefa, tempo, selecionado, completado, id}: ITarefa){
+interface Props extends ITarefa{
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+export default function item(
+    {
+        tarefa, tempo, selecionado, completado, id, selecionaTarefa
+    }: Props){
     
     return(
-        <li className={style.item}>
+        <li 
+            className={`${style.item} ${selecionado ? style.itemSelecionado : ''} ${completado ? style.itemCompletado : ''}`} 
+            onClick={() => !completado && selecionaTarefa({
+                tarefa,
+                tempo,
+                selecionado,
+                completado,
+                id
+            })}
+        >
             <h3>{tarefa}</h3>
             <span>{tempo}</span>
+            {completado && <span className={style.concluido} aria-label="tarefa completada!"></span>}
         </li>
     )
 }
--- to-do-list/src/Pages/App.tsx
+++ to-do-list/src/Pages/App.tsx
@@ -3,15 +3,48 @@ import { Cronometro } from '../Components/Cronometro';
 import Formulario from '../Components/Formulario';
 import Lista from '../Components/Lista';
 import { ITarefa } from '../Types/tarefa';
-import style from './App.module.scss'
+import style from './App.module.scss';
 
 function App() {
-  const [tarefas, setTarefas] = useState<ITarefa[]>([])
+
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa){
+    setSelecionado(tarefaSelecionada)
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })));
+  }
+
+  function finalizarTarefa(){
+    if(selecionado){
+      setSelecionado(undefined)
+      setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => {
+        if(tarefa.id === selecionado.id){
+          return{
+              ...tarefa,
+            selecionado: false,
+            completado: true
+          }
+        }
+        return tarefa
+      }))
+    }
+  }
+
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas}/>
-      <Lista tarefas = {tarefas}/>
-      <Cronometro/>
+      <Lista 
+        tarefas = {tarefas}
+        selecionaTarefa = {selecionaTarefa}
+      />
+      <Cronometro 
+        selecionado={selecionado}
+        finalizarTarefa={finalizarTarefa}
+      />
     </div>
   );
 }
